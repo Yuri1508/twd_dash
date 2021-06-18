5e1da5962ba9b8f1a3e028336b2459a159704c7b
Yuri1508 <yurisolsagara60@gmail.com>
2021-06-17 14:11:04 -1000

    News Files

--- class/api.class.php
+++ class/api.class.php
@@ -1,6 +1,7 @@
 <?php
 
     class twd {
+
         public function getData($Dstart,$Dend){
 
         $url = "http://api.aremiti.net:31415/rest-api/aremiti/horaires?date_debut=". $Dstart ."&date_fin=". $Dend;
@@ -12,10 +13,36 @@
         curl_setopt($ch, CURLOPT_URL,$url);
         // Execute
         $j_data=curl_exec($ch);
-        // // Closing
-        curl_close($ch);
+        // Closing
+        // curl_close($ch);
 
-        return $j_data;
+        
+        $t_result = json_decode($j_data,true);
+
+        // get all time 
+        foreach($t_result[$Dstart]["T"] as $stime => $value){
+
+            $t_time['T'][] = $stime;
+        }
+
+        foreach($t_result[$Dstart]["M"] as $stime => $value){
+
+            $t_time['M'][] = $stime;
+        }
+
+        // build array t_data
+        foreach($t_time['T'] as $v){
+
+            $t_data['data']['T'][] = $t_result[$Dstart]["T"][$v];
+        }
+
+        foreach($t_time['M'] as $v){
+
+            $t_data['data']['M'][] = $t_result[$Dstart]["M"][$v];
+        }
+
+
+        return json_encode($t_data);
         }
     }
 
